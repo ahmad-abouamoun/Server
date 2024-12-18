1cec1ddd314c3c79a25c9c5ebb9dea8ca65de24a
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-18 14:06:08 +0200

    imported the mongoose library

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import connectToDatabase from "./connect.js";
 const app = express();
 
 app.listen(8080, () => {
     console.log("hello world");
+    connectToDatabase();
 });
