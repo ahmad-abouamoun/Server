5ceb3d9060940ee464cded7cdd71ceffa4a79563
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-19 15:58:46 +0200

    added the Route folder

--- connect.js
+++ connect.js
@@ -3,9 +3,11 @@ import {connect} from "mongoose";
 const connectToDatabase = async () => {
     try {
         await connect("mongodb://localhost:27017/BalanceBeam");
+
         console.log("Connected to db");
     } catch (error) {
         console.log(error);
     }
 };
+
 export default connectToDatabase;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import connectToDatabase from "./connect.js";
 const app = express();
 
-app.listen(8080, () => {
+app.listen(8080, async () => {
     console.log("hello world");
-    connectToDatabase();
+    await connectToDatabase();
 });
