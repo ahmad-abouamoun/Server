f4309fc1faccd64db9b50b773e378990484df5a3
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-19 16:12:43 +0200

    created the getExperts api

--- Controller/userController.js
+++ Controller/userController.js
@@ -4,3 +4,7 @@ export const getUsers = async (req, res) => {
     const users = await User.find({type: "user"});
     return res.json(users);
 };
+export const getExperts = async (req, res) => {
+    const users = await User.find({$or: [{type: "coach"}, {type: "nutritionist"}, {type: "therapist"}]});
+    return res.json(users);
+};
