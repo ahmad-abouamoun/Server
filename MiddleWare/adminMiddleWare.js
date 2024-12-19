0d7f917148eed63837fdb103fc19342025972936
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-19 18:07:20 +0200

    added a try catch block in the admin middleware

--- MiddleWare/adminMiddleWare.js
+++ MiddleWare/adminMiddleWare.js
@@ -1,6 +1,13 @@
+import {verify} from "jsonwebtoken";
+
 const adminMiddleWare = (req, res, next) => {
     const {token} = req.body;
     if (!token) {
         return res.status(400).json({message: "token was not provided"});
+    }
+    try {
+        const decode = verify(token, secretKey);
+    } catch {
+        return res.status(400).json({message: "Invalid token"});
     }
 };
