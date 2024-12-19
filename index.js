b427a20ed885589943b2f8cf3cf3e708aedff7ce
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-19 16:08:31 +0200

    used the userroute with app

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import connectToDatabase from "./connect.js";
+import userRoute from "./Route/userRoute.js";
 const app = express();
+
+app.use("/users", userRoute);
 
 app.listen(8080, async () => {
     console.log("hello world");
