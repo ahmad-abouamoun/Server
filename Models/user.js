42f761eb91cebfad7ba031076e89a9e44fc2737e
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-19 16:01:54 +0200

    created the user model

---
+++ Models/user.js
@@ -0,0 +1,42 @@
+import {Schema, model} from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    banned: {
+        type: Boolean,
+        required: true,
+    },
+    filename: {
+        type: String,
+        required: true,
+    },
+    diabetes: {
+        type: Boolean,
+        required: true,
+    },
+    highCholesterol: {
+        type: Boolean,
+        required: true,
+    },
+    hypertension: {
+        type: Boolean,
+        required: true,
+    },
+});
+
+export const User = model("User", userSchema);
