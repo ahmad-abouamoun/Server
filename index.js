d641ec1861da86b3ab4b43f5755c17a832d3185a
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-20 01:01:16 +0200

    used the food router in the index

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import connectToDatabase from "./connect.js";
 import userRoute from "./Route/userRoute.js";
+import foodRoute from "./Route/foodRoute.js";
+
 const app = express();
 app.use(express.json());
 
 app.use("/users", userRoute);
+app.use("/food", foodRoute);
 
 app.listen(8080, async () => {
     console.log("hello world");
