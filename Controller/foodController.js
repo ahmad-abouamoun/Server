f875ceedb554002d19a09e2092969ceb71299856
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-20 00:52:28 +0200

    handled the case if any variable was not provided

--- Controller/foodController.js
+++ Controller/foodController.js
@@ -1,3 +1,6 @@
 export const createFood = async (req, res) => {
-    const {name, description, diabetes, hypertension, highCholesterol} = req.body;
+    const {name, description, diseases} = req.body;
+    if (!name || !description || !diseases) {
+        return res.status(400).json({message: `${name} ${description} ${diabetes} ${hypertension} ${highCholesterol}`});
+    }
 };
