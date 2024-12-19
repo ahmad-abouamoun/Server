26ac4d7dfd29c3b68f63e785c5d70b894d6b2200
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-19 16:15:46 +0200

    added comments to the apis to know their functionality in the future

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,9 +1,12 @@
 import {User} from "../Models/user.js";
 
+//gets the user type users
 export const getUsers = async (req, res) => {
     const users = await User.find({type: "user"});
     return res.json(users);
 };
+
+//gets users with coach and nutritionist and therapist type
 export const getExperts = async (req, res) => {
     const users = await User.find({$or: [{type: "coach"}, {type: "nutritionist"}, {type: "therapist"}]});
     return res.json(users);
