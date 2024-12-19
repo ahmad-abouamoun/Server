1edfb102b750fd94ba8539ea6b8bb9fade727848
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-20 00:53:40 +0200

    implemented the logic to create a new food

--- Controller/foodController.js
+++ Controller/foodController.js
@@ -3,4 +3,15 @@ export const createFood = async (req, res) => {
     if (!name || !description || !diseases) {
         return res.status(400).json({message: `${name} ${description} ${diabetes} ${hypertension} ${highCholesterol}`});
     }
+    const newFood = new Food({
+        name,
+        description,
+        diseases,
+        filename: req.file.filename,
+    });
+
+    await newFood.save();
+    res.status(201).json({
+        newFood,
+    });
 };
