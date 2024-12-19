14dcb48af52977839740db71bf29338f0fd61248
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-20 00:54:12 +0200

    surrounded the logic of the createFood with a try catch block

--- Controller/foodController.js
+++ Controller/foodController.js
@@ -1,17 +1,24 @@
 export const createFood = async (req, res) => {
     const {name, description, diseases} = req.body;
-    if (!name || !description || !diseases) {
-        return res.status(400).json({message: `${name} ${description} ${diabetes} ${hypertension} ${highCholesterol}`});
-    }
-    const newFood = new Food({
-        name,
-        description,
-        diseases,
-        filename: req.file.filename,
-    });
+    try {
+        if (!name || !description || !diseases) {
+            return res
+            .status(400)
+            .json({message: `${name} ${description} ${diabetes} ${hypertension} ${highCholesterol}`});
+        }
+        const newFood = new Food({
+            name,
+            description,
+            diseases,
+            filename: req.file.filename,
+        });
 
-    await newFood.save();
-    res.status(201).json({
-        newFood,
-    });
+        await newFood.save();
+        res.status(201).json({
+            newFood,
+        });
+    } catch (error) {
+        console.error("Error creating food:", error.message);
+        res.status(500).json({message: "Internal Server Error while creating the food"});
+    }
 };
