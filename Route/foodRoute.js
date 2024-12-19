9626e3bf42346ac3209b3ab2d8113dfa2a101674
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-20 00:56:17 +0200

    used the multer library to save files in the backend

--- Route/foodRoute.js
+++ Route/foodRoute.js
@@ -2,5 +2,15 @@ import {Router} from "express";
 import {createFood} from "../Controller/foodController";
 
 const router = new Router();
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "Images/userImages");
+    },
+    filename: (req, file, cb) => {
+        const email = req.body.email;
+        const fileExtension = file.originalname.split(".").pop();
+        cb(null, `${email}.${fileExtension}`);
+    },
+});
 
 router.post("/", createFood);
