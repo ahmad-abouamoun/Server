7621fff365cfc19f0d3c5ff817e972bfa5684874
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-19 17:10:21 +0200

    used multer library to updload files

--- Route/userRoute.js
+++ Route/userRoute.js
@@ -1,7 +1,18 @@
 import {Router} from "express";
 import {createUser, getExperts, getUsers, Signin} from "../Controller/userController.js";
+import multer from "multer";
 
 const router = new Router();
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "Images/userImages");
+    },
+    filename: (req, file, cb) => {
+        const email = req.body.email;
+        const fileExtension = file.originalname.split(".").pop();
+        cb(null, `${email}.${fileExtension}`);
+    },
+});
 
 router.get("/", getUsers);
 router.get("/experts", getExperts);
