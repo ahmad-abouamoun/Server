8700e2e164077c935d15a7a9eaa48162eff78a61
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-19 16:11:56 +0200

    created the /experts route in user router

--- Route/userRoute.js
+++ Route/userRoute.js
@@ -2,5 +2,8 @@ import {Router} from "express";
 import {getUsers} from "../Controller/userController.js";
 
 const router = new Router();
+
 router.get("/", getUsers);
+router.get("/experts", getExperts);
+
 export default router;
