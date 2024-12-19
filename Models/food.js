6b3fe7479fe391329da20cb94c42ffa1d652ceff
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-20 00:45:28 +0200

    created the food schema

---
+++ Models/food.js
@@ -0,0 +1,32 @@
+import {Schema, model} from "mongoose";
+
+const foodSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    diseases: {
+        diabetes: {
+            type: Boolean,
+            required: true,
+        },
+        highCholesterol: {
+            type: Boolean,
+            required: true,
+        },
+        hypertension: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    filename: {
+        type: String,
+        required: true,
+    },
+});
+
+export const Food = model("Food", foodSchema);
