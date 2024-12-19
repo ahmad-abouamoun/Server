95cadf7d915d5efd3269175a16b2f95141c6a93d
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-19 16:28:45 +0200

    added the json plugin

--- Route/userRoute.js
+++ Route/userRoute.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import {getExperts, getUsers} from "../Controller/userController.js";
+import {getExperts, getUsers, Signin} from "../Controller/userController.js";
 
 const router = new Router();
 
 router.get("/", getUsers);
 router.get("/experts", getExperts);
+
+router.post("/signin", Signin);
 
 export default router;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectToDatabase from "./connect.js";
 import userRoute from "./Route/userRoute.js";
 const app = express();
+app.use(express.json());
 
 app.use("/users", userRoute);
 
