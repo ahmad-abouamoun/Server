8e170315d821806f5f1e7a48ff14865c72d2c2b1
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-21 16:45:12 +0200

    created the program schema

---
+++ Models/programs.js
@@ -0,0 +1,22 @@
+import {Schema, model} from "mongoose";
+
+const programSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    training: {
+        type: [String],
+        default: [],
+    },
+    link: {
+        type: [String],
+        default: [],
+    },
+    filename: {
+        type: String,
+        required: true,
+    },
+});
+
+export const Program = model("Program", programSchema);
