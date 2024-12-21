37a57e19294ceae82d34d0381a51e02fa1935c9f
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-21 14:11:22 +0200

    created the logic to handle the situation if not all fields were given in the create meeting api

--- Controller/meetingController.js
+++ Controller/meetingController.js
@@ -1,3 +1,7 @@
 export const createMeeting = async (req, res) => {
+    const {token, title, startDate, endDate, expert} = req.body;
+    if (!token || !title || !startDate || !endDate || !expert) {
+        return res.status(400).json({message: "all fields should be provided"});
+    }
     res.status(200).json({message: "CreateMeeting api"});
 };
