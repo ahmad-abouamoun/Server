fae04009a222a4fe6379cea6f4cda1d5c1cc8314
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-21 14:02:06 +0200

    imported the create meeting router onto the index

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import connectToDatabase from "./connect.js";
 import userRoute from "./Route/userRoute.js";
 import foodRoute from "./Route/foodRoute.js";
+import meetingRoute from "./Route/foodRoute.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/users", userRoute);
 app.use("/food", foodRoute);
+app.use("/meetings", meetingRoute);
 
 app.listen(8080, async () => {
     console.log("hello world");
