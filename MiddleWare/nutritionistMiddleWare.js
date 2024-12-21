1e58904654c9d62036cbdfe9c2de777035baf124
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-21 13:33:11 +0200

    created the logic to get the token from the req and handled the case where the token was not provided

--- MiddleWare/nutritionistMiddleWare.js
+++ MiddleWare/nutritionistMiddleWare.js
@@ -1,4 +1,9 @@
 import jwt from "jsonwebtoken";
 import {secretKey} from "../Controller/userController.js";
 
-export const nutritionist = (req, res, next) => {};
+export const nutritionistMiddleWare = (req, res, next) => {
+    const {token} = req.body;
+    if (!token) {
+        return res.status(400).json({message: "token was not provided"});
+    }
+};
