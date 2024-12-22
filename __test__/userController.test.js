0530cdaa45bf9be4203ac19ee52e15e242a85835
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-22 15:44:42 +0200

    started working on test2 for the get users api

--- __test__/userController.test.js
+++ __test__/userController.test.js
@@ -46,4 +46,37 @@ describe("getUsers", () => {
         expect(User.find).toHaveBeenCalledWith({type: "user"});
         expect(res.json).toHaveBeenCalledWith(mockUsers);
     });
+    it("should return only the user with user type 'user' ", async () => {
+        const mockUsers = [
+            {
+                _id: "1",
+                name: "John Doe",
+                type: "user",
+                password: "hashedPassword123",
+                email: "johndoe@example.com",
+                banned: false,
+                filename: "profile1.png",
+                diseases: {
+                    diabetes: false,
+                    highCholesterol: false,
+                    hypertension: false,
+                },
+            },
+            {
+                _id: "2",
+                name: "Jane Smith",
+                type: "admin",
+                password: "hashedPassword456",
+                email: "janesmith@example.com",
+                banned: false,
+                filename: "profile2.png",
+                diseases: {
+                    diabetes: true,
+                    highCholesterol: true,
+                    hypertension: false,
+                },
+            },
+        ];
+        User.find.mockResolvedValue(mockUsers.filter((user) => user.type === "user"));
+    });
 });
