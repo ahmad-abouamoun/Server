396cc34cebf7584ff3940ae1a67dc41fef80c216
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-22 16:13:36 +0200

    finalized the getExperts second test

--- __test__/UserConrollerTests/getExperts.test.js
+++ __test__/UserConrollerTests/getExperts.test.js
@@ -3,7 +3,7 @@ import {User} from "../../Models/user.js";
 
 jest.mock("../../Models/user.js");
 
-describe("getUsers", () => {
+describe("getExperts", () => {
     it('should return only users with type "coach","therapist",or "nutritionist"', async () => {
         const mockUsers = [
             {
@@ -90,8 +90,46 @@ describe("getUsers", () => {
                 },
             },
         ];
+
+        const expectedOutput = [
+            {
+                _id: "1",
+                name: "John Doe",
+                type: "coach",
+                password: "hashedPassword123",
+                email: "johndoe@example.com",
+                banned: false,
+                filename: "profile1.png",
+                diseases: {
+                    diabetes: false,
+                    highCholesterol: false,
+                    hypertension: false,
+                },
+            },
+            {
+                _id: "2",
+                name: "Jane Smith",
+                type: "nutritionist",
+                password: "hashedPassword456",
+                email: "janesmith@example.com",
+                banned: false,
+                filename: "profile2.png",
+                diseases: {
+                    diabetes: true,
+                    highCholesterol: true,
+                    hypertension: false,
+                },
+            },
+        ];
         User.find.mockResolvedValue(
             mockUsers.filter((user) => ["coach", "therapist", "nutritionist"].includes(user.type))
         );
+        const req = {};
+        const res = {json: jest.fn()};
+
+        await getExperts(req, res);
+
+        expect(User.find).toHaveBeenCalledWith({$or: [{type: "coach"}, {type: "nutritionist"}, {type: "therapist"}]});
+        expect(res.json).toHaveBeenCalledWith(expectedOutput);
     });
 });
