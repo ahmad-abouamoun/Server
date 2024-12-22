7f17fa74831bbfdd60fffdeb09d124de3d47e06e
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-22 17:34:55 +0200

    started working on the tests of creating a user

---
+++ __test__/UserConrollerTests/createUser.test.js
@@ -0,0 +1,8 @@
+import {createUser} from "../../Controller/userController.js";
+import {User} from "../../Models/user.js";
+import fs from "fs";
+import bcrypt from "bcrypt";
+
+describe("create Users", () => {
+    it("it should create a user", async () => {});
+});
