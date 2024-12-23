68d8586d51b9e73ac215813b1dfae71756482418
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 16:43:38 +0200

    created the route for programs

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import connectToDatabase from "./connect.js";
 import userRoute from "./Route/userRoute.js";
 import foodRoute from "./Route/foodRoute.js";
 import meetingRoute from "./Route/meetingRoute.js";
-
+import programsRoute from "./Route/programsRoute.js";
 const app = express();
 app.use(express.json());
 
 app.use("/users", userRoute);
 app.use("/food", foodRoute);
 app.use("/meetings", meetingRoute);
+app.use("/programs", programsRoute);
 
 app.listen(8080, async () => {
     console.log("hello world");
