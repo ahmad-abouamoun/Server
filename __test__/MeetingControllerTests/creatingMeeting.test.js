608eaf8318e456cc3938576d7b93daab649b86d0
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 11:14:44 +0200

    started working on tests of the create meeting to handle case where not all fields were provided

--- __test__/MeetingControllerTests/creatingMeeting.test.js
+++ __test__/MeetingControllerTests/creatingMeeting.test.js
@@ -2,3 +2,7 @@ import {createMeeting} from "../../Controller/meetingController.js";
 import {Meeting} from "../../Models/meeting.js";
 
 jest.mock("../../Models/meeting.js");
+
+describe("create meeting", () => {
+    it("should return status code 400 if not all fields were provided", async () => {});
+});
