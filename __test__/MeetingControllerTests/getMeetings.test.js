aa1440b8abea181def18fcd45e759ed9737c58ee
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 14:15:47 +0200

    finalized the test i was creating

--- __test__/MeetingControllerTests/getMeetings.test.js
+++ __test__/MeetingControllerTests/getMeetings.test.js
@@ -5,6 +5,8 @@ import {secretKey} from "../../Controller/userController.js";
 jest.mock("../../Models/meeting.js");
 
 describe("get meetings", () => {
+    const decodedToken = {id: "1"};
+
     const mockToken = jwt.sign({id: "1", type: "user"}, secretKey);
     const res = {status: jest.fn().mockReturnThis(), json: jest.fn()};
 
@@ -14,7 +16,14 @@ describe("get meetings", () => {
         expect(res.status).toHaveBeenCalledWith(400);
         expect(res.json).toHaveBeenCalledWith({message: "token was not provided"});
     });
-    it("should return 400 if an error occuerd", async () => {
-        const req = {body: {token: mockToken}};
+    it("should return 400 if token was not valid", async () => {
+        const req = {body: {token: "mockToken"}};
+        jwt.verify = jest.fn().mockImplementation(() => {
+            throw new Error("invalid token");
+        });
+        await getMeetings(req, res);
+        expect(jwt.verify).toHaveBeenCalledWith("mockToken", expect.any(String));
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({message: "an error has occured"});
     });
 });
