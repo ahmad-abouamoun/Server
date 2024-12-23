1c263bcd1a2425a4bc4720ba77af91187ea975b9
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 06:13:13 +0200

    created the req and made the res obj accessable throughtout the whole test

--- __test__/FoodControllerTests/createFood.test.js
+++ __test__/FoodControllerTests/createFood.test.js
@@ -5,12 +5,21 @@ import {Food} from "../../Models/food";
 jest.mock("../../Models/food");
 
 describe("create food", () => {
+    const res = {status: jest.fn().mockReturnThis(), json: jest.fn()};
     it("should return status code 400 if not all fields were provided", async () => {
         const req = {body: {name: "", desription: "", diseases: "{}"}};
-        const res = {status: jest.fn().mockReturnThis(), json: jest.fn()};
         await createFood(req, res);
         expect(res.json).toHaveBeenCalledWith({message: `all fileds should be provided`});
         expect(res.status).toHaveBeenCalledWith(400);
     });
-    it("should return status code 500 if error occured in the Db", async () => {});
+    it("should return status code 500 if error occured in the Db", async () => {
+        const req = {
+            body: {
+                name: "strawberry",
+                desription: "very delicious",
+                diseases: '{"diabetes": false, "highCholesterol": false, "hypertension": false}',
+                filename: "strawberry.png",
+            },
+        };
+    });
 });
