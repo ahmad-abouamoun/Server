64ad9f79bb8e006be669b23ade3f523ffd62c694
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 06:37:15 +0200

    started working on the tests for the get foods api and working on the case where an error in db occured

--- __test__/FoodControllerTests/getFoods.test.js
+++ __test__/FoodControllerTests/getFoods.test.js
@@ -2,3 +2,7 @@ import {createFood} from "../../Controller/foodController";
 import {Food} from "../../Models/food";
 
 jest.mock("../../Models/food");
+
+describe("get foods", () => {
+    it("should return status code 500 if an error in the db occured", async () => {});
+});
