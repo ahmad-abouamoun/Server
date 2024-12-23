d53bf1691f1e004acf9dfd8481e94981aab6e010
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 06:45:14 +0200

    called the aoi getFoods

--- __test__/FoodControllerTests/getFoods.test.js
+++ __test__/FoodControllerTests/getFoods.test.js
@@ -1,13 +1,18 @@
 import {json} from "express";
-import {createFood} from "../../Controller/foodController";
+import {getFoods} from "../../Controller/foodController";
 import {Food} from "../../Models/food";
 
 jest.mock("../../Models/food");
 
 describe("get foods", () => {
     const req = {};
-    const res = {status: jest.fn().mockReturnThis, json: jest.fn()};
+    const res = {status: jest.fn().mockReturnThis(), json: jest.fn()};
+
     it("should return status code 500 if an error in the db occured", async () => {
-        Food.find.mockedRejectedValue(new Error("error in the Db"));
+        Food.find.mockRejectedValue(new Error("error in the Db"));
+        await getFoods(req, res);
+        expect(res.json).toHaveBeenCalledWith({message: "Internal Server Error while fetching the food"});
+
+        expect(res.status).toHaveBeenCalledWith(500);
     });
 });
