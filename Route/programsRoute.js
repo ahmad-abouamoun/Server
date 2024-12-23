4a60456e089af0b634cf9b79b9e5b90fe6024e15
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 17:44:46 +0200

    used the multer library to accept files in create progran

--- Route/programsRoute.js
+++ Route/programsRoute.js
@@ -1,6 +1,19 @@
 import {Router} from "express";
 import {createProgram} from "../Controller/programsController.js";
-
+import multer from "multer";
+//
 const router = new Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "Images/programsImages");
+    },
+    filename: (req, file, cb) => {
+        const name = req.body.name;
+        const fileExtension = file.originalname.split(".").pop();
+        cb(null, `${name}.${fileExtension}`);
+    },
+});
+
 router.post("/", createProgram);
 export default router;
