ad5f1cc20a732fa9aa8c8e4ebb66d5fe31463c9e
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 13:58:58 +0200

    started working on the test if not token was provided

--- __test__/MeetingControllerTests/getMeetings.test.js
+++ __test__/MeetingControllerTests/getMeetings.test.js
@@ -2,3 +2,12 @@ import jwt from "jsonwebtoken";
 import {getMeetings} from "../../Controller/meetingController.js";
 import {Meeting} from "../../Models/meeting.js";
 jest.mock("../../Models/meeting.js");
+
+describe("get meetings", () => {
+    const mockToken = jwt.sign({id: "1", type: "user"}, secretKey);
+    const res = {status: jest.fn().mockReturnThis(), json: jest.fn()};
+
+    it("should return 400 if token does not exist", async () => {
+        const req = {body: {token: ""}};
+    });
+});
