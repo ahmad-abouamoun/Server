762b68aa1fa8e60e24f06ce38cfd0ffb70e1263e
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 06:39:43 +0200

    created the req and res obj

--- __test__/FoodControllerTests/getFoods.test.js
+++ __test__/FoodControllerTests/getFoods.test.js
@@ -1,8 +1,11 @@
+import {json} from "express";
 import {createFood} from "../../Controller/foodController";
 import {Food} from "../../Models/food";
 
 jest.mock("../../Models/food");
 
 describe("get foods", () => {
+    const req = {};
+    const res = {status: jest.fn().mockReturnThis, json: jest.fn()};
     it("should return status code 500 if an error in the db occured", async () => {});
 });
