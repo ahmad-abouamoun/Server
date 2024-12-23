8c63feefab11f8a553ac0c9141c26d90e850f1ab
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 06:03:44 +0200

    started working on the first test for handling the case if not all fields were provided

--- __test__/FoodControllerTests/createFood.test.js
+++ __test__/FoodControllerTests/createFood.test.js
@@ -2,3 +2,7 @@ import {createFood} from "../../Controller/foodController";
 import {Food} from "../../Models/food";
 
 jest.mock("../../Models/food");
+
+describe("create food", () => {
+    it("should return status code 400 if not all fields were provided", async () => {});
+});
