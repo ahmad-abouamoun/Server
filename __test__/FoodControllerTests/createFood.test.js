95f545232527d751cc9ac433e2dcf42dac06602b
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 06:05:40 +0200

    created the req and res obj in the test

--- __test__/FoodControllerTests/createFood.test.js
+++ __test__/FoodControllerTests/createFood.test.js
@@ -1,8 +1,12 @@
+import {json} from "express";
 import {createFood} from "../../Controller/foodController";
 import {Food} from "../../Models/food";
 
 jest.mock("../../Models/food");
 
 describe("create food", () => {
-    it("should return status code 400 if not all fields were provided", async () => {});
+    it("should return status code 400 if not all fields were provided", async () => {
+        const req = {body: {}};
+        const res = {status: jest.fn().mockReturnThis(), json: jest.fn()};
+    });
 });
