68d1b653440e6b80a4884958dc0c1aceb5750cb7
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 06:08:51 +0200

    finished test nb 1

--- __test__/FoodControllerTests/createFood.test.js
+++ __test__/FoodControllerTests/createFood.test.js
@@ -6,7 +6,10 @@ jest.mock("../../Models/food");
 
 describe("create food", () => {
     it("should return status code 400 if not all fields were provided", async () => {
-        const req = {body: {}};
+        const req = {body: {name: "", desription: "", diseases: "{}"}};
         const res = {status: jest.fn().mockReturnThis(), json: jest.fn()};
+        await createFood(req, res);
+        expect(res.json).toHaveBeenCalledWith({message: `all fileds should be provided`});
+        expect(res.status).toHaveBeenCalledWith(400);
     });
 });
