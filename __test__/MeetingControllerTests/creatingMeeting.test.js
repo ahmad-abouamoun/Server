db061840685bfdf1a81ff2898933b995da8af9b0
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 11:17:30 +0200

    created the req and res obj

--- __test__/MeetingControllerTests/creatingMeeting.test.js
+++ __test__/MeetingControllerTests/creatingMeeting.test.js
@@ -1,8 +1,12 @@
+import {json} from "express";
 import {createMeeting} from "../../Controller/meetingController.js";
 import {Meeting} from "../../Models/meeting.js";
 
 jest.mock("../../Models/meeting.js");
 
 describe("create meeting", () => {
-    it("should return status code 400 if not all fields were provided", async () => {});
+    const res = {status: jest.fn().mockReturnThis(), json: jest.fn()};
+    it("should return status code 400 if not all fields were provided", async () => {
+        const req = {body: {token: "", startDate: "", endDate: "", expert: "", title: ""}};
+    });
 });
