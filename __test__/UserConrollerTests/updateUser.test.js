e2238e8bb2699c90e7df27bc66e5db70065bdafb
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-23 04:00:31 +0200

    finished the 1st test for the update user

--- __test__/UserConrollerTests/updateUser.test.js
+++ __test__/UserConrollerTests/updateUser.test.js
@@ -14,5 +14,31 @@ describe("update User", () => {
             params: {id: 1},
         };
         const res = {status: jest.fn().mockReturnThis(), json: jest.fn()};
+
+        const mockUser = {
+            _id: "1",
+            name: "John Doe",
+            type: "user",
+            password: "hashedPassword123",
+            email: "johndoe@example.com",
+            banned: false,
+            filename: "profile1.png",
+            diseases: {
+                diabetes: false,
+                highCholesterol: false,
+                hypertension: false,
+            },
+            save: jest.fn().mockResolvedValue(true),
+        };
+        User.findByIdAndUpdate.mockResolvedValue(mockUser);
+        await updateUser(req, res);
+
+        expect(User.findByIdAndUpdate).toHaveBeenCalledWith(
+            1,
+            {name: "John", diseases: '{"diabetes": true, "highCholesterol": true, "hypertension": true}'},
+            {new: true}
+        );
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({message: "user was updated"});
     });
 });
