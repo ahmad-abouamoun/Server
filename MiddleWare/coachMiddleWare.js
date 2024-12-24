1cb489732b766961b95ad2ec43cc114cea8e7e7f
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-24 03:16:49 +0200

    finalzied the coach middleware and added it into the routes that needs auth

--- MiddleWare/coachMiddleWare.js
+++ MiddleWare/coachMiddleWare.js
@@ -6,4 +6,14 @@ export const coachMiddleWare = (req, res, next) => {
     if (!token) {
         return res.status(400).json({message: "token was not provided"});
     }
+    try {
+        const decode = jwt.verify(token, secretKey);
+        if (decode.type === "coach") {
+            next();
+        } else {
+            return res.status(400).json({message: "unAutharized"});
+        }
+    } catch (error) {
+        return res.status(400).json({message: "Invalid token ", error});
+    }
 };
--- Route/programsRoute.js
+++ Route/programsRoute.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
 import {createProgram, getPrograms, updateProgram} from "../Controller/programsController.js";
 import multer from "multer";
+import {coachMiddleWare} from "../MiddleWare/coachMiddleWare.js";
 
 const router = new Router();
 
@@ -17,12 +18,12 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage});
-//route that uses the multer library to recieve a file and invokes the create program api
-router.post("/", upload.single("file"), createProgram);
+//route that uses the multer library to recieve a file and invokes the create program api and only user type coach is allowed to use it
+router.post("/", upload.single("file"), coachMiddleWare, createProgram);
 
 //route that invokes the get programs api
 router.get("/", getPrograms);
 
-//route that invokes the update program api
-router.patch("/:id", updateProgram);
+//route that invokes the update program api and only user type coach is allowed to use it
+router.patch("/:id", coachMiddleWare, updateProgram);
 export default router;
