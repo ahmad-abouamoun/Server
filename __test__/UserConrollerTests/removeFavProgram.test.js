b191e0de99f417a84bfaac348ed0693b3722f3d6
Ahmad Abou Amoun <aabouamoun@gmail.com>
2025-01-09 03:56:08 +0200

    finished the first test for the case a Db error pccured

--- __test__/UserConrollerTests/removeFavProgram.test.js
+++ __test__/UserConrollerTests/removeFavProgram.test.js
@@ -1,9 +1,8 @@
 import jwt from "jsonwebtoken";
-import {removeFavProgram} from "../../Controller/userController.js";
+import {addFavProgram, removeFavProgram} from "../../Controller/userController.js";
 import {secretKey} from "../../Controller/userController.js";
 
 import {User} from "../../Models/user.js";
-import jwt from "jsonwebtoken";
 
 jest.mock("../../Models/user");
 describe("remove favorite program", () => {
@@ -13,4 +12,13 @@ describe("remove favorite program", () => {
             token: jwt.sign({id: 1}, secretKey),
         },
     };
+    const res = {status: jest.fn().mockReturnThis(), json: jest.fn()};
+    it("should return status code 500 incase of error in DB", async () => {
+        User.findById.mockRejectedValue(null);
+        await addFavProgram(req, res);
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({
+            message: "Something went wrong",
+        });
+    });
 });
